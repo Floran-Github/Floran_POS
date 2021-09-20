8b72258d3652e7a4b6049340fb5da5036e110d22
Ehtesham Siddiqui <siddiquie.ehtesham@gmail.com>
2021-09-20 17:36:54 +0530

    Product Detail Feature

--- frontend/src/components/Pages/product/Productpage.js
+++ frontend/src/components/Pages/product/Productpage.js
@@ -1,96 +1,92 @@
-import React, { Component, Fragment } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { getProducts} from '../../../actions/product'
-import { Link, Redirect } from 'react-router';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getProducts } from '../../../actions/product';
+import { Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export class Productpage extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
 
-    static proptypes = {
-        products: PropTypes.array.isRequired,
-        getProducts: PropTypes.func.isRequired,
-        isAuthenticated: PropTypes.bool,
+  static proptypes = {
+    products: PropTypes.array.isRequired,
+    getProducts: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+  };
+
+  componentDidMount() {
+    this.props.getProducts();
+  }
+
+  redirectToProductDetail = (product) => {
+    this.props.history.push('/product/detail', { product: product });
+  };
+
+  render() {
+    var productList;
+    if (this.props.products.length === 0 && this.props.products[0] === 0) {
+      productList = [];
+    } else {
+      productList = this.props.products;
     }
 
-    componentDidMount(){
-        this.props.getProducts();
-    }
+    return (
+      <Fragment>
+        <div className='col-12'>
+          <div className='row'>
+            <div className='col-md-10 col-6'>
+              <h2>Products</h2>
+            </div>
+            <div className='col-md-2 col-6'>
+              <a href='/product/create' className='btn btn-secondary'>
+                <i className='fas fa-plus'></i> New Product
+              </a>
+            </div>
+          </div>
+          <div className='table-responsive'>
+            <table
+              className='table table-bordered table-hover'
+              data-page-length='100'
+            >
+              <thead className='table-light'>
+                <tr>
+                  <th>S/N</th>
+                  <th>Name</th>
+                  <th>Quantity</th>
+                  <th>Price</th>
+                </tr>
+              </thead>
+              <tbody className='table-light'>
+                {this.props.products.map((product, index) => (
+                  <tr
+                    key={product.id}
+                    data-item={index}
+                    onClick={(e) => this.redirectToProductDetail(product)}
+                  >
+                    <td>{index + 1}</td>
+                    <td>{product.product_name}</td>
+                    <td>{product.product_quantity}</td>
+                    <td>{product.product_price}</td>
+                  </tr>
 
-    redirectToProductDetail = (e) => {
-        const prdid = e.currentTarget.getAttribute('data-item');
-        console.log('clllae ',prdid);
-        // <Redirect to={`/product/detail/${prdid}`} />
-        <Redirect to="/product/detail" />
-    }
-   
-    render() {
-        
-        var productList;
-        if(this.props.products.length === 0 && this.props.products[0] === 0){
-            productList = []
-        } else {
-            productList = this.props.products
-        }
-
-        return (
-
-
-            <Fragment>
-                <div className="col-12">
-                    <div className="row">
-                        <div className="col-md-10 col-6">
-                            <h2>
-                                Products
-                            </h2>
-                        </div>
-                        <div className="col-md-2 col-6">
-                            <a href="/product/create" className='btn btn-secondary'><i className="fas fa-plus"></i> New Product</a>
-                        </div>
-                    </div>
-                    <div className="table-responsive">
-                        <table className="table table-bordered table-hover" data-page-length='100'>
-                            <thead className="table-light">
-                                <tr>
-                                    <th>S/N</th>
-                                    <th>Name</th>
-                                    <th>Quantity</th>
-                                    <th>Price</th>
-                                </tr>
-                            </thead>
-                            <tbody className="table-light">
-
-                               {this.props.products.map((product,index) => (
-                                   
-                                        <tr key={product.id} data-item={index} onClick={this.redirectToProductDetail}>
-                                            <td>{index + 1}</td>
-                                            <td>{product.product_name}</td>
-                                            <td>{product.product_quantity}</td>
-                                            <td>{product.product_price}</td>    
-                                        </tr>
-                               ))}     
-                            
-
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </Fragment>
-        )
-    }
+                  // </Link>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-const mapStateToProps = state => ({
-    products: state.product.products,
-    isAuthenticated: state.auth.isAuthenticated
-})
+const mapStateToProps = (state) => ({
+  products: state.product.products,
+  isAuthenticated: state.auth.isAuthenticated,
+});
 
-export default connect(mapStateToProps,{getProducts})(Productpage)
-
-
-
-
-
-
-
-
-
+export default connect(mapStateToProps, { getProducts })(Productpage);
